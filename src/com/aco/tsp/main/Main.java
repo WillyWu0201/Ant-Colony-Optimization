21d60f01402fcc916a5822d39d6ba74fe47e8771
Wu, Chia-Wei <willy@loftechs.com>
2018-06-24 16:19:01 +0800

    調整參數

--- src/com/aco/tsp/main/Main.java
+++ src/com/aco/tsp/main/Main.java
@@ -5,11 +5,11 @@ import com.aco.tsp.algorithms.TravelingSalesman;
 public class Main {
 
 	// Dorigo等人建議，螞蟻數量S = 節點總數量 A
-	private static int ants = 100; // 螞蟻數量
-	private static int iterator = 1000; // 迭代次數
+	private static int ants = 50; // 螞蟻數量
+	private static int iterator = 100; // 迭代次數
 	private static int pheromoneImpactRate = 1; // 費洛蒙對決策的影響
 	private static int distanceImpactRate = 5; // 距離對決策的影響
-	private static double evaporationRate = 0.5;// 費洛蒙蒸發率
+	private static double evaporationRate = 0.9;// 費洛蒙蒸發率
 
 	public static void main(String[] args) {
 		System.out.println("------------------ANT COLONY OPTIMIZATION------------------");
